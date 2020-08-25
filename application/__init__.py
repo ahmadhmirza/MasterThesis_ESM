60e1665a418dc606865c0f70ed1ab60003c1532b
ahmadhmirza <ahmadhasanmirza@gmail.com>
2020-08-25 21:57:56 +0200

    registerd services

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 27 08:46:51 2020
+Created on Fri Mar 27 09:46:51 2020
 
-@author: ahmad
+@author: Ahmad H. Mirza
 
 __init__.py file is considered a package, and can be imported. When you import 
 a package, the __init__.py executes and defines what symbols the package 
 exposes to the outside world.
-"""
 
-
-"""
 command to run :
     flask run --host=0.0.0.0  # runs on internal network
     flask run  # runs on localhost
@@ -19,16 +16,24 @@ command to run :
 
 """initialize app"""
 from flask import Flask
+
+"""
+# uncomment this block if machine learning services are to be enabled
+# for now they are disabled/commented out (ML_ClassificationService, ML_App)
 try:
     import tensorflow as tf
     print("TF IMPORT SUCCESSFUL")
 except Exception as e:
     print("INIT: " + str(e))
+
+"""
 def create_app():
     import os
     app = Flask(__name__)
+    #The secret key is used for generating encrypted keys, api-keys and signatures.
     app.config['SECRET_KEY'] = "SECRET_KEY_STRING_HERE"
     
+    #All new services that are to be added to HyAPI platform should be registerd in this block
     with app.app_context():
         from .FileUpload import FileUpload_routes
         app.register_blueprint(FileUpload_routes.fileUpload_bp)
@@ -51,7 +56,16 @@ def create_app():
         from .ML_App import ML_GUI_Routes
         app.register_blueprint(ML_GUI_Routes.mlGUI_bp)
 
-        #For each new added service, it blueprint should be registered
+        from .CompuMethodsService import CompuMethod_routes
+        app.register_blueprint(CompuMethod_routes.compuMethod_bp)
+
+        from .DataStreamingService import DataStream_routes
+        app.register_blueprint(DataStream_routes.dataStream_bp)
+
+        from .ImageIntegrationDemo import ImageIntegration_routes
+        app.register_blueprint(ImageIntegration_routes.imgIntegration_bp)
+        
+        #For each new added service, its blueprint should be registered
         # in this function.
         #[Reference] https://hackersandslackers.com/flask-blueprints
         return app
